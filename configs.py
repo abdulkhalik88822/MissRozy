29fff4b25cfc2096861e91faed822a5d39b533a1
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2024-08-06 16:49:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -42,7 +42,7 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME" , "@File_Press_bot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", -1001569815531))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6133440326"))
-	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://abdulmk1556:abdulmk@cluster0.otuwovy.mongodb.net/?retryWrites=true&w=majority")
+	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://abdulmk184:abdulmk184@cluster0.fv4ffn1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001954276464")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001626107740")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
